1d6da89d70ac7782e2149a6bd2c895b47e9bac83
Angelo Loza <angelo_384@hotmail.com>
2015-09-17 21:02:46 -0500

    class Node separated

---
+++ Node.java
@@ -0,0 +1,13 @@
+
+public class Node {
+    
+    int data;
+    Node next;
+    Node previous;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.previous = null;
+    }
+}
